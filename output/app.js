4a6649cd71e50f6965ac51586b3c2081bfd9689e
Samsul kobir <89704576+skpixels@users.noreply.github.com>
2022-12-22 23:44:49 +0600

    Explicit & Union

--- output/app.js
+++ output/app.js
@@ -1,25 +1,49 @@
 "use strict";
+// types system in typescript 
+// --------------------------------------
 // function age(a: number,b:number){
 //     return a+b
 // }
 // console.log(age(1,5));
 // arrays 
-// let fruits = ['mango', 'banana', 'apple', 'orange']
-let price = [2, 3, 6, 2, 3, '', {}];
-// fruits.push('coconuts')
-// price.push(13)
-// price.push('13')
-// price.push('another')
+/* let fruits = ['mango', 'banana', 'apple', 'orange']
+let price = [2,3,6,2,3, '', {}]
+
+fruits.push('coconuts')
+price.push(13)
+price.push('13')
+price.push('another')
 price.push({
     name: 'kabir',
     age: 22
 });
-// console.log(price);
+console.log(price); */
 // object 
-const myObj = {
+/* const myObj = {
     name: 'shakib',
     age: 33,
     isCaptain: true
+}
+
+myObj.isCaptain = false
+console.log(myObj); */
+// Explicit & Union
+// ----------------------------
+//  variable 
+/* let a: number | string | boolean
+a = 33
+a = true
+a = 'string' */
+// arrays 
+/* let myArr: (string| number)[] = []
+myArr.push(33, 'string')
+console.log(myArr); */
+// Object 
+let myObj;
+myObj = {
+    name: 'sk',
+    age: 22,
+    isSingle: true,
+    name2: 'pixel'
 };
-myObj.isCaptain = false;
 console.log(myObj);
--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,66 @@
+// types system in typescript 
+// --------------------------------------
+
 // function age(a: number,b:number){
 //     return a+b
 // }
 // console.log(age(1,5));
 
 
-// arrays 
 
-// let fruits = ['mango', 'banana', 'apple', 'orange']
+// arrays 
+/* let fruits = ['mango', 'banana', 'apple', 'orange']
 let price = [2,3,6,2,3, '', {}]
 
-// fruits.push('coconuts')
-// price.push(13)
-// price.push('13')
-// price.push('another')
+fruits.push('coconuts')
+price.push(13)
+price.push('13')
+price.push('another')
 price.push({
     name: 'kabir',
     age: 22 
 });
-
-// console.log(price);
+console.log(price); */
 
 
 // object 
-
-const myObj = {
+/* const myObj = {
     name: 'shakib',
     age: 33,
     isCaptain: true
 }
 
 myObj.isCaptain = false
+console.log(myObj); */
+
+
+// Explicit & Union
+// ----------------------------
+
+//  variable 
+/* let a: number | string | boolean
+a = 33
+a = true
+a = 'string' */
+
+// arrays 
+/* let myArr: (string| number)[] = []
+myArr.push(33, 'string')
+console.log(myArr); */
+
+// Object 
+
+let myObj:{
+    name: string,
+    age: number,
+    isSingle: boolean,
+    name2: string
+}
+
+myObj ={
+    name: 'sk',
+    age: 22,
+    isSingle: true,
+    name2: 'pixel'
+}
 console.log(myObj);
