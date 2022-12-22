95343c9d6f68328c76f2df670dff38f7a7638911
Samsul kobir <89704576+skpixels@users.noreply.github.com>
2022-12-22 23:25:44 +0600

    added type ststem

--- output/app.js
+++ output/app.js
@@ -1,5 +1,25 @@
 "use strict";
-function age(a, b) {
-    return a + b;
-}
-console.log(age(1, 5));
+// function age(a: number,b:number){
+//     return a+b
+// }
+// console.log(age(1,5));
+// arrays 
+// let fruits = ['mango', 'banana', 'apple', 'orange']
+let price = [2, 3, 6, 2, 3, '', {}];
+// fruits.push('coconuts')
+// price.push(13)
+// price.push('13')
+// price.push('another')
+price.push({
+    name: 'kabir',
+    age: 22
+});
+// console.log(price);
+// object 
+const myObj = {
+    name: 'shakib',
+    age: 33,
+    isCaptain: true
+};
+myObj.isCaptain = false;
+console.log(myObj);
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,33 @@
-function age(a: number,b:number){
-    return a+b
+// function age(a: number,b:number){
+//     return a+b
+// }
+// console.log(age(1,5));
+
+
+// arrays 
+
+// let fruits = ['mango', 'banana', 'apple', 'orange']
+let price = [2,3,6,2,3, '', {}]
+
+// fruits.push('coconuts')
+// price.push(13)
+// price.push('13')
+// price.push('another')
+price.push({
+    name: 'kabir',
+    age: 22 
+});
+
+// console.log(price);
+
+
+// object 
+
+const myObj = {
+    name: 'shakib',
+    age: 33,
+    isCaptain: true
 }
-console.log(age(1,5));
+
+myObj.isCaptain = false
+console.log(myObj);
